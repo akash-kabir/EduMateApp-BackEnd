97a12ceeecf7c390b96044c756589b06e583b843
Mirza Akash Kabir <nashim564@gmail.com>
2025-10-25 23:37:11 +0530

    Role Refresh

--- models/User.js
+++ models/User.js
@@ -7,17 +7,19 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, default: 'student' },
+  role: { type: String, default: 'Student' }, // default role
+}, {
+  timestamps: true,
 });
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-userSchema.methods.matchPassword = async function (enteredPassword) {
+userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
