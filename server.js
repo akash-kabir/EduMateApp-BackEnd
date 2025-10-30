d5c736304d3a1cf8f0e2890631b01c0d6974b084
Mirza Akash Kabir <nashim564@gmail.com>
2025-10-30 23:58:22 +0530

    comment out

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-require('dotenv').config();
+//require('dotenv').config();
 
 const app = express();
 connectDB();
